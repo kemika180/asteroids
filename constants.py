02eac0f113ffb9b1615fda04c45e138966aa5090
kemika180 <jkgurchiek@gmail.com>
2025-11-29 22:15:10 -0700

    added player motion

--- constants.py
+++ constants.py
@@ -1,2 +1,11 @@
+# screen dimensions
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# drawing line widths
+LINE_WIDTH = 2
+
+# player constants
+PLAYER_RADIUS = 20
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 from logger import log_state
 import pygame
 
@@ -8,14 +9,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        log_state()
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time/1000
 
 
 if __name__ == "__main__":
